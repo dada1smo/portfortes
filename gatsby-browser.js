2af71fa2f8c0042c14c4ee2505c2f50ddf2ade93
Adalberto Sampaio <sampaio.afn@gmail.com>
2020-10-08 17:11:58 -0300

    Retrying to fix fontawesome

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,3 @@
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
 // normalize CSS across browsers
 import "./src/normalize.css"
 import "./src/global.scss"
@@ -11,3 +8,9 @@ import "prismjs/themes/prism.css"
 // Fonts
 require("typeface-space-mono");
 require("typeface-archivo");
+
+// This ensures that the icon CSS is loaded immediately before attempting to render icons
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+// Prevent fontawesome from dynamically adding its css since we did it manually above
+config.autoAddCss = false;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,5 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-
-// This ensures that the icon CSS is loaded immediately before attempting to render icons
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-// Prevent fontawesome from dynamically adding its css since we did it manually above
-config.autoAddCss = false;
-
 import Bio from "../components/bio"
 import SEO from "../components/seo"
 import Home from "../templates/home"
